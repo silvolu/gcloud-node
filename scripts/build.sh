6f852082000146697ed0771cfdefd1b5a8fdfdc1
Silvano Luciani <silvano.luciani@gmail.com>
2014-08-28 17:00:43 -0700

    Iteration

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -ev
 
 # if on master and not a pull request, execute regression tests
-if [ "${TRAVIS_BRANCH}" eq "master" && "${TRAVIS_PULL_REQUEST}" -ne "false" ]; then
+if [[ "${TRAVIS_BRANCH}" eq "master" && "${TRAVIS_PULL_REQUEST}" -ne "false" ]]; then
   npm run regression-test
 else
   echo "Branch: ${TRAVIS_BRANCH}"
